e8346636f90b2a1475d9537099bb26cd61feab69
aahhmmeeddaammr <aamr24987@gmail.com>
2024-09-02 23:37:00 +0300

    Done\

---
+++ Assignment2(Views)/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2_Views_.Controllers
+{
+	public class AccountController : Controller
+	{
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		public IActionResult SignUp() {
+			return View();
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Assignment2(Views)/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2_Views_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult AboutUs() {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
